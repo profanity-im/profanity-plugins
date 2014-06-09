859fd2cfbf5dff1c50a4686497e359146239d7cf
James Booth <boothj5@gmail.com>
2014-06-09 22:42:18 +0100

    Added /say command to say.py

--- say.py
+++ say.py
@@ -1,14 +1,27 @@
 import prof
 import os
 
+on_message = False
+
 def prof_on_message_received(jid, message):
-    os.system("say \"" + jid + " says " + message + "\" 2>/dev/null")    
+    if on_message:
+        os.system("say \"" + jid + " says " + message + "\" 2>/dev/null")    
     return message
 
 def prof_on_room_message_received(room, nick, message):
-    os.system("say \"" + nick + " says " + message + " in " + room + "\" 2>/dev/null")    
+    if on_message:
+        os.system("say \"" + nick + " says " + message + " in " + room + "\" 2>/dev/null")    
     return message
 
 def prof_on_private_message_received(room, nick, message):
-    os.system("say \"" + nick + " says " + message + "\" 2>/dev/null")    
+    if on_message:
+        os.system("say \"" + nick + " says " + message + "\" 2>/dev/null")    
     return message
+
+def _cmd_say(msg=None):
+    if msg: 
+        os.system("say \"" + msg + "\" &2>/dev/null")    
+
+def prof_init(version, status):
+    prof.register_command("/say", 0, 1, "/say message", "Say something.", "Say something.",
+        _cmd_say)
