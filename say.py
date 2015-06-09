c8781665aa103c02916739d39f765699eb2caee3
James Booth <boothj5@gmail.com>
2015-06-10 00:46:05 +0100

    Added enable arg to say.py

--- say.py
+++ say.py
@@ -1,27 +1,36 @@
 import prof
 import os
 
-on_message = True
+enabled = False
 
 def prof_post_chat_message_display(jid, message):
-    if on_message:
+    global enabled
+    if enabled:
         os.system("say \"" + jid + " says " + message + "\" 2>/dev/null")    
     return message
 
 def prof_post_room_message_display(room, nick, message):
-    if on_message:
+    global enabled
+    if enabled:
         os.system("say \"" + nick + " says " + message + " in " + room + "\" 2>/dev/null")    
     return message
 
 def prof_post_priv_message_display(room, nick, message):
-    if on_message:
+    global enabled
+    if enabled:
         os.system("say \"" + nick + " says " + message + "\" 2>/dev/null")    
     return message
 
-def _cmd_say(msg=None):
-    if msg: 
-        os.system("say \"" + msg + "\" 2>/dev/null")    
+def _cmd_say(arg=None):
+    global enabled
+    if arg == "on":
+        enabled = True
+    elif arg == "off":
+        enabled = False
+    else:
+        prof.cons_show("Usage: /say on|off")
 
 def prof_init(version, status):
-    prof.register_command("/say", 0, 1, "/say message", "Say something.", "Say something.",
+    prof.register_command("/say", 1, 1, "/say on|pff", "Enable or disable say.", "Enable or disable say.",
         _cmd_say)
+    prof.register_ac("/say", [ "on", "off" ])
