7796e8df21c861f2c165635ed77c0c64f7bdbc37
James Booth <boothj5@gmail.com>
2016-02-18 22:02:22 +0000

    Updated /pid plugin help

--- pid/pid.c
+++ pid/pid.c
@@ -18,5 +18,10 @@ cmd_pid(char **args)
 void
 prof_init(const char * const version, const char * const status)
 {
-    prof_register_command("/pid", 0, 0, "/pid", "Get process ID", "Get process ID", cmd_pid);
+    const char *synopsis[] = { "/pid", NULL };
+    const char *description = "Show process ID and parent Process ID in the console window.";
+    const char *args[][2] = { { NULL, NULL } };
+    const char *examples[] = { NULL };
+
+    prof_register_command("/pid", 0, 0, synopsis, description, args, examples, cmd_pid);
 }
