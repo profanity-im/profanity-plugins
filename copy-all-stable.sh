7145ae16e5ac0c6f80107f8edebf94bcb3c740f0
James Booth <boothj5@gmail.com>
2016-02-28 23:38:37 +0000

    Added helper script

---
+++ copy-all-stable.sh
@@ -0,0 +1,22 @@
+rm -f ~/.local/share/profanity/plugins/*
+
+cp ascii.py ~/.local/share/profanity/plugins/.
+cp browser.py ~/.local/share/profanity/plugins/.
+cp platform-info.py ~/.local/share/profanity/plugins/.
+cp say.py ~/.local/share/profanity/plugins/.
+cp whoami.py ~/.local/share/profanity/plugins/.
+cp tests/python-test.py ~/.local/share/profanity/plugins/.
+
+cd pid
+make clean
+make
+cp pid.so ~/.local/share/profanity/plugins/.
+cd ..
+
+cd tests/test-c-plugin
+make clean
+make
+cp test-c-plugin.so ~/.local/share/profanity/plugins/.
+cd ../..
+
+
