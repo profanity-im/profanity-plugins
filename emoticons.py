5faccde200050a33b45ac24dea765f48229cf90e
James Booth <boothj5@gmail.com>
2016-03-16 20:32:34 +0000

    Fixed emoticons.py to encode as utf8 string

--- emoticons.py
+++ emoticons.py
@@ -2,10 +2,10 @@ import prof
 
 def _emote(input_str):
     result = input_str
-    result = result.replace(":-)", u'\u263a')
-    result = result.replace(":)", u'\u263a')
-    result = result.replace(":-(", u'\u2639')
-    result = result.replace(":(", u'\u2639')
+    result = result.replace(":-)", u'\u263a'.encode("utf-8"))
+    result = result.replace(":)", u'\u263a'.encode("utf-8"))
+    result = result.replace(":-(", u'\u2639'.encode("utf-8"))
+    result = result.replace(":(", u'\u2639'.encode("utf-8"))
     return result
 
 def prof_pre_chat_message_display(jid, message):
