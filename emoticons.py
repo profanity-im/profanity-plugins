dfd7b09d9e009cd3d15058f6723f96cd7552efe9
James Booth <boothj5@gmail.com>
2016-07-20 00:56:45 +0100

    Make emoticons.py python2 and 3 compatible

--- emoticons.py
+++ emoticons.py
@@ -3,14 +3,15 @@ Convert smileys ( :) :( etc) to the Unicode character representation
 """
 
 import prof
+from builtins import chr
 
 
 def _emote(input_str):
     result = input_str
-    result = result.replace(":-)", u'\u263a'.encode("utf-8"))
-    result = result.replace(":)", u'\u263a'.encode("utf-8"))
-    result = result.replace(":-(", u'\u2639'.encode("utf-8"))
-    result = result.replace(":(", u'\u2639'.encode("utf-8"))
+    result = result.replace(":-)", chr(9786))
+    result = result.replace(":)", chr(9786))
+    result = result.replace(":-(", chr(9785))
+    result = result.replace(":(", chr(9785))
     return result
 
 
